3388f06a980f295ac7a422254c84ad634386a666
Soumojit Chatterjee <sc@Soumojit-PC>
2025-04-04 21:21:48 +0530

    raft_module.cpp added

---
+++ code/storage_node/raft_module.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <set>
+#include <optional>
+#include <string>
+using namespace std;
+class Raft
+{
+public:
+    enum Role
+    {
+        FOLLOWER,
+        CANDIDATE,
+        LEADER
+    };
+    enum Operation
+    {
+        GET,
+        SET,
+        DEL
+    };
+    enum Message
+    {
+        VoteRequest,
+        VoteResponse,
+        LogRequest,
+        LogResponse
+    };
+    struct LogEntry
+    {
+        int term;
+        string msg;
+
+        LogEntry(int t, const string &m) : term(t), msg(m) {}
+    };
+    struct Follower
+    {
+        int replicaID;
+        int AZ_ID;
+    };
+    // Constructor
+    Raft(int numPeers, vector<Follower> &followers) : currentTerm(0),
+                                                      votedFor(nullopt),
+                                                      commitLength(0),
+                                                      currentRole(FOLLOWER),
+                                                      currentLeader(nullopt),
+                                                      votesReceived(),
+                                                      sentLength(numPeers, 0),
+                                                      ackedLength(numPeers, 0)
+    {
+    }
+
+private:
+    int currentTerm;
+    optional<int> votedFor;
+    vector<LogEntry> log;
+    int commitLength;
+    Role currentRole;
+    optional<int> currentLeader;
+    set<int> votesReceived;
+    vector<int> sentLength;
+    vector<int> ackedLength;
+};
